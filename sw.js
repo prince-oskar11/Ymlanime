7f4ce2bedb6b12852da8c25e3298aba6bf241987
prince-oskar11 <prince-oskar11@hotmail.com>
2026-04-09 14:54:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-self.addEventListener('install', () => {
-  console.log('SW installed');
-});
-self.addEventListener('fetch', () => {
-  // optional cache logic
-});
+const cacheName='ymlanime-cache-v1';
+const assets=['/','/index.html','/manifest.json','/icon.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(assets)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)))});
